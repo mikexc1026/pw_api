4dbc81472dbb77e80841bd179a0fec585910ac79
brinkmanm <mbrinkman1026@gmail.com>
2021-07-13 19:59:12 +0000

    add cors package

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const routes = require('./routes/routes');
 const app = express();
 const port = 8000;
 
+app.use(cors({ origin: "http://localhost:8000" }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true,
-}));
+app.use(bodyParser.urlencoded({ extended: true, }));
 
 routes(app);
 
